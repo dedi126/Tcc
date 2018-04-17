182445dc1c4cd897b348845466ceaa87cb2fa9fe
dedi126 <dedi.akakom@gmail.com>
2018-04-17 15:16:45 +0700

    tambah index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php>
+<html>
+	<body>
+		echo "WELCOME TO SAYA";
+		echo "<br/>";
+		echo "Praktikum Teknologi Cloud Computing";
+		echo "<br/>";
+		echo "NIM : 155610126";
+		echo "<br/>";
+		echo "NAMA : DEDI SETIAWAN";
+	</body>
+</html>
+?>
